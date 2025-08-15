d8ea05734d611a69ff2e3320c79d97e17642e0ac
ndvuongq04 <nguyendinhvuong08122004@gmail.com>
2025-08-15 16:32:19 +0700

    init project

---
+++ src/main/java/vn/ndv/vocab_webapp/VocabWebappApplication.java
@@ -0,0 +1,13 @@
+package vn.ndv.vocab_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/ndv/vocab_webapp/VocabWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ndv.vocab_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
